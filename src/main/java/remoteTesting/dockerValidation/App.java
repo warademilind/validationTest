7c7629af2091fa98b9d62d0734dbbdd5142e098c
warademilind <30415682+warademilind@users.noreply.github.com>
2021-06-19 16:08:24 +0530

    Update App.java
    Moved selenium mail class

--- src/main/java/remoteTesting/dockerValidation/App.java
+++ src/main/java/remoteTesting/dockerValidation/App.java
@@ -1,5 +1,17 @@
 package remoteTesting.dockerValidation;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
 /**
  * Hello world!
  *
@@ -8,6 +20,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        		// TODO Auto-generated method stub
+DesiredCapabilities cap=DesiredCapabilities.chrome();
+		URL u=new URL("http://localhost:4444/wd/hub");
+		RemoteWebDriver driver=new RemoteWebDriver(u,cap);
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		takeSnapShot(driver, "screenshot/test.png") ;		
+	}
+	public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+		//Convert web driver object to TakeScreenshot
+		TakesScreenshot scrShot =((TakesScreenshot)new EventFiringWebDriver(webdriver));
+		//Call getScreenshotAs method to create image file
+		File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+		//Move image file to new destination
+		File DestFile=new File(fileWithPath);
+		//Copy file at destination
+		FileUtils.copyFile(SrcFile, DestFile);
+		}
     }
 }
